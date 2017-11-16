c589f0d3807bb52e5bfec90f2f9ef505ea52da31
Dave Cariello <drcariel@us.ibm.com>
2017-11-16 11:32:20 -0600

    readme and uninstall sh consistency

--- packages/uninstall.sh
+++ packages/uninstall.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
+#
+# use the command line interface to uninstall standard actions deployed
+# automatically
+#
+# To run this command
+# ./uninstall.sh  <AUTH> <EDGE_HOST> <WSK_CLI>
+# AUTH and EDGE_HOST are found in $HOME/.wskprops
+# WSK_CLI="$OPENWHISK_HOME/bin/wsk"
 
 set -e
 set -x
-: ${OPENWHISK_HOME:?"OPENWHISK_HOME must be set and non-empty"}
-WSK_CLI="$OPENWHISK_HOME/bin/wsk"
 
 if [ $# -eq 0 ]
 then
-    echo "Usage: ./uninstall.sh $EDGE_HOST $AUTH"
+    echo "Usage: ./uninstall.sh <auth> <edgehost> <pathtowskcli>"
 fi
 
-EDGE_HOST="$1"
-AUTH="$2"
+AUTH="$1"
+EDGE_HOST="$2"
+WSK_CLI="$3"
+
+# If the auth key file exists, read the key in the file. Otherwise, take the
+# first argument as the key itself.
+if [ -f "$AUTH" ]; then
+    AUTH=`cat $AUTH`
+fi
+
+PACKAGE_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export WSK_CONFIG_FILE= # override local property file to avoid namespace clashes
+
 
 echo Uninstalling Template Package \
 
